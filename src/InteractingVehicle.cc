ac7ce24e2928f1369ada877569c552e586c56039
flmi622b <503-flmi622b@users.noreply.gitlab.mn.tu-dresden.de>
2021-12-06 13:59:17 +0000

    prototyping, a bit too xtreme

--- src/InteractingVehicle.cc
+++ src/InteractingVehicle.cc
@@ -13,6 +13,7 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
+#include <vector>
 #include "InteractingVehicle.h"
 
 Define_Module(InteractingVehicle);
@@ -21,6 +22,8 @@ InteractingVehicle::~InteractingVehicle() {
     cancelAndDelete(sendPSEvt);
 }
 
+// TODO:
+// Annahme: Es bremst immer das langsamere auto, wenn beide gleichschnell sind, das, was eine höhere id hat
 void InteractingVehicle::initialize(int stage)
 {
     DemoBaseApplLayer::initialize(stage);
@@ -30,6 +33,83 @@ void InteractingVehicle::initialize(int stage)
         // initialize pointers to other modules
         if (veins::FindModule<veins::TraCIMobility*>::findSubModule(getParentModule())) {
             mobility = veins::TraCIMobilityAccess().get(getParentModule());
+            traci = mobility->getCommandInterface(); // hier gib es getRouteIds und getPlannedRouteIds
+            traciVehicle = mobility->getVehicleCommandInterface(); // hier gibt es getLanePosition()
+
+            // getRoadId()
+            // oder getLaneId
+            // getLanePosition
+            // getPlannedRoadIds
+            // wir brauchen noch eine methode, um zu entscheiden, wann er wo sein wird (Move kann getPositionAt),
+            // wir können so eine zeit berechnen, wann er am "anfang" eines vectors und wann am "ende" eines vectors ist, um so zu vergleichen ob es im selben
+            //   zeitradius ist
+            // dass dann in ein tuple mit dem vector aufnehmen
+            //   dann die schneidenden vectors (schneidefunktion gibt es nicht, aber siehe tracicommandinterface.h->getDistance)
+            //   und die zeit vergleichen und ein threshhold einbauen
+            // so kann dann erkannt werden ob die autos crashen
+
+            // wir brauchen die lane, um die shape zu bekommen
+
+            // calculate meeting points for the street we are on and every street we are goint to approach
+            //std::vector<std::string> road_ids = { traciVehicle.getRoadId() };
+
+            std::vector<std::string> possible_lanes;
+
+            possible_lanes.push_back(traciVehicle->getLaneId());
+
+            /*EV << "planning to use roads: ";
+            for(std::string road : traciVehicle->getPlannedRoadIds()) {
+                EV << road;
+            }
+            EV << std::endl;*/
+
+            //auto* first = new veins::TraCICommandInterface::Road(traci, traciVehicle->getRoadId());
+            EV << "my lanes are: " << std::endl;
+            for(std::string l : possible_lanes) {
+                //possible_lanes.push_back(l);
+                EV << l << " ";
+
+                auto lane = veins::TraCICommandInterface::Lane(traci, l);
+                EV << "mean speed: " << lane.getMeanSpeed() << " ";
+                //lane.getMeanSpeed();
+                //EV << "shape: " << lane.getShape() << " ";
+
+                //lane.getShape();
+            }
+            EV << std::endl;
+
+
+            // getLaneIds()
+            /*
+            {
+                auto* road = new veins::TraCICommandInterface::Road(traci, traciVehicle->getRoadId());
+                auto* move = new veins::Move();
+                //move->setDirectionByTarget();
+                move->setSpeed(road->getMeanSpeed());
+            }
+            */
+
+            //road_ids
+
+
+            /*for(string t : traciVehicle->getRoadIds()) {
+
+            }*/
+
+            //drive_positions
+
+
+            // vielleicht mit einem Move arbeiten?
+            // oder mit Heading?
+
+            // wie bekommt man die road?
+            // man kann road -> lanes -> getShape (Alles in traciCommandInterface)
+
+            /***
+             * Offene Fragen:
+             * - TraCICommandInterface::Vehicle::stopAt() soll ein radius mitgegeben werden, der nie benutzt wird. Wieso soll dieser mitgegeben werden?
+             */
+
         }
 
         psInterval = par("psInterval");
--- src/InteractingVehicle.h
+++ src/InteractingVehicle.h
@@ -16,6 +16,8 @@
 #ifndef __INTERACTING_VEHICLE_H_
 #define __INTERACTING_VEHICLE_H_
 
+#include <tuple>
+#include <vector>
 #include <omnetpp.h>
 #include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
 #include "InterVehicleMessage_m.h"
@@ -25,6 +27,12 @@
 
 using veins::Coord;
 using veins::DemoBaseApplLayer;
+
+
+// would be cool to be a real class, like in opengl
+typedef std::tuple<double, double, double> vec3d;
+typedef std::tuple<double, double, double> point3d;
+
 
 /**
  * @brief A class for vehicles in SUMO simulations.
@@ -74,7 +82,36 @@ protected:
      */
     //virtual InterVehicleMessage* populateMsg();
 
+    // Deprecated, too complex:
+        // vectors, the car will drive in the future
+        // todo: delete the already driven vectors?
+        // holds <vector, start_time, end_time>
+        //std::vector<std::tuple<vec3d, simtime_t_cref>> drive_positions_time;
+        // holds the possible crash/meeting carnames, positions and times
+        //std::vector<std::tuple<std::string, vec3d, simtime_t_cref>> potencial_meets;
+
+        // wir haben für die berechnung der position nur die koordinaten und speed,
+        // also müssen wir daraus die straße ableiten, wenn möglich (ansonsten den vektor nehmen der gegeben ist)
+        // und so die meeting points erstellen
+        // da es so nur 2 punkte gibt, müssen wir also checken, ob wir uns treffen, wenn wir den vektor länger machen
+        // wenn nicht -> gut, wenn ja -> meeting point
+
+    // Annahme: Es kann sich nur immer an Kreuzungen, e.g. am Ende einer Straße getroffen werden
+    // potential_meeting_points holds the end of the street as a crossing and therefore a potential meeting point, and the time it will be there
+    // This simulates a perfect world, where a road is ending as soon as a crossing occours
+    // in the real world, we would have to determine all road-crossings first
+    // as I don't know how far this project will go, it will already be a vector, so not too much additional work would be needed
+    std::vector<std::tuple<point3d, simtime_t_cref>> potential_meeting_points;
+
+    // meetings holds <name, coordinates, time> the calculated meetings between two cars which would normally result in a crash
+    std::vector<std::tuple<std::string, point3d, simtime_t_cref>> meetings;
+
+
     veins::TraCIMobility* mobility;
+    veins::TraCICommandInterface* traci;
+    veins::TraCICommandInterface::Vehicle* traciVehicle;
+
+
     simtime_t psInterval;
     /* messages for periodic events, namly the position/speed update transmissions */
     cMessage* sendPSEvt;
